4af2c8fdb76720d2ed88904d7924093b055b37c0
MrQuentin <quentin.levasseur@hei.yncrea.fr>
2017-10-18 14:52:41 +0200

    update genreDaoImpl

--- src/main/java/hei/devweb/javacinee/dao/impl/GenreDaoImpl.java
+++ src/main/java/hei/devweb/javacinee/dao/impl/GenreDaoImpl.java
@@ -1,9 +1,48 @@
 package hei.devweb.javacinee.dao.impl;
 
 import hei.devweb.javacinee.dao.GenreDao;
+import hei.devweb.javacinee.entities.Genre;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenreDaoImpl implements GenreDao {
 
 
+    @Override
+    public List<Genre> listGenres() {
+        List<Genre> listOfGenres = new ArrayList<>();
+        String querry = "SELECT * FROM genre ORDER BY name";
+        try (Connection connection = DataSourceProvider.getDataSource().getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                try (ResultSet results =
+                             statement.executeQuery(querry)) {
+                    while (results.next()) {
+                       Genre genre = new Genre(
+                                results.getInt("genre_id"),
+                                results.getString("name"));
+                        listOfGenres.add(genre);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            // Manage Exception
+            e.printStackTrace();
+        }
+        return listOfGenres;
+    }
 
+    @Override
+    public Genre getGenre(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void addGenre(String nom) {
+
+    }
 }
